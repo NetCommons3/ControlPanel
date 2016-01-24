9f10b9a401168ceee5eb79d44c66a95333aabb78
s-nakajima <nakajimashouhei@gmail.com>
2016-01-24 22:21:34 +0900

    取得する際、URLの生死確認する処理の修正ミス

--- Controller/ControlPanelController.php
+++ Controller/ControlPanelController.php
@@ -57,7 +57,7 @@ class ControlPanelController extends ControlPanelAppController {
 				$this->Session->write('getNotificationError', true);
 				//サイトの生死確認
 				if ($this->Notification->ping()) {
-					$this->Notification->serializeXmlToArray();
+					$notifications = $this->Notification->serializeXmlToArray();
 					//更新処理
 					$this->Notification->updateNotifications(array(
 						'Notification' => $notifications
